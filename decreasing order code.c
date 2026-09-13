c409aeb3b2a1aa978d8cacd1030a3434193c6def
NeilThakkar15 <neil.thakkar11b2425@gmail.com>
2026-09-13 10:53:40 +0530

    Update decreasing order code.c

--- decreasing order code.c
+++ decreasing order code.c
@@ -1,28 +1,32 @@
-
 #include <stdio.h>
 
 int main() {
-    int a,b,c;
+    int a, b, c;
+    
     printf("Enter 3 numbers: ");
-    scanf("%d%d%d", &a,&b,&c);
-    if (a>b && a>c) {
-        if (b>c)
-            printf("Decreasing order: %d > %d > %d\n", a,b,c);
-        else 
-            printf("Decreasing order: %d > %d > %d\n", a,c,b);
-}
-    else if (b>c && b>a) {
-        if (c>a)
-            printf("Decreasing order: %d > %d > %d\n", b,c,a);
-        else
-            printf("Decreasing order: %d > %d > %d\n", b,a,c);
-}
+    scanf("%d%d%d", &a, &b, &c);
+    
+    if (a > b && a > c) {
+        if (b > c) {
+            printf("Decreasing order: %d > %d > %d\n", a, b, c);
+        } else {
+            printf("Decreasing order: %d > %d > %d\n", a, c, b);
+        }
+    } 
+    else if (b > c && b > a) {
+        if (c > a) {
+            printf("Decreasing order: %d > %d > %d\n", b, c, a);
+        } else {
+            printf("Decreasing order: %d > %d > %d\n", b, a, c);
+        }
+    } 
     else {
-        if (b>a)
-            printf("Decreasing order: %d > %d > %d\n", c,b,a);
-        else 
-            printf("Decreasing order: %d > %d > %d\n", c,a,b);
+        if (b > a) {
+            printf("Decreasing order: %d > %d > %d\n", c, b, a);
+        } else {
+            printf("Decreasing order: %d > %d > %d\n", c, a, b);
+        }
     }        
 
-return 0;
+    return 0;
 }
